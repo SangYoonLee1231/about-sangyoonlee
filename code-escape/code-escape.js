8c929b993dc0af6da02e854ba3851f1c8deb1178
SangYoonLee <prestro1231@hufs.ac.kr>
2022-10-04 04:38:15 +0900

    Update All
    2022-10-04 자기소개 페이지
    # Code Escape 미니 게임 제작 (비동기 함수 개념으로 인해 잠시 중단)

--- code-escape/code-escape.js
+++ code-escape/code-escape.js
@@ -1,10 +1,69 @@
-const txt = document.querySelector(".txt");
+const root = document.querySelector(".root");
 
-const startMessage = `여느 때와 마찬가지로 나는 내 방에서 분명 잠이 들었을 텐데..
+function showMessage(div, message) {
+  let index = 0;
+
+  let typing = () => {
+    console.log(message.length);
+    if (message[index] === "\n") {
+      div.innerHTML += "<br/>";
+    } else {
+      div.innerHTML += message[index];
+    }
+    index += 1;
+  };
+  if (index > message.length) {
+    return;
+  }
+
+  let typingTimper = setInterval(typing, 50);
+  setTimeout(() => {
+    clearInterval(typingTimer);
+  }, 50 * message.length);
+}
+
+function sleep(ms) {
+  const loopTime = Date.now() + ms;
+  while (Date.now() < loopTime) {}
+}
+
+// OPENING
+
+const openingDiv = document.querySelector(".opening-div");
+
+const openingMessage = `\n여느 때와 마찬가지로 나는 내 방에서 분명 잠이 들었을 텐데..
 \n지금 나의 눈앞에 보이는 낯선 풍경은 어째서인가\n현실과는 다른 괴리감이 느껴진다.`;
 
-let index = 0;
+console.log(openingDiv);
 
-const opening = `코드 이스케이프 미니 게임에 오신 여러분을 환영합니다.\n
-주어진 수수께기를 풀고 올바른 코드(암호)를 입력해 이 곳을 탈출하세요.\n
-그럼 첫 번째 문제입니다.`;
+showMessage(openingDiv, openingMessage);
+
+// CODE 1
+
+const startDiv = document.querySelector(".start-div");
+
+const startMessage = `
+\n코드 이스케이프 미니 게임에 오신 여러분을 환영합니다.\n
+주어진 수수께기를 풀고 올바른 코드(암호)를 입력해 이 곳을 탈출하세요.\n`;
+
+//sleep(2000);
+
+console.log(startDiv);
+
+showMessage(startDiv, startMessage);
+
+function showCode(message, codeNum) {
+  const codeDiv = document.createElement("div");
+
+  const code = `
+    <div class="code${codeNum} fade-in">
+      <p>${message}</p>
+      <img src="code_img/code-${codeNum}.png" width="550"></img>
+    </div>
+  `;
+
+  codeDiv.innerHTML = code;
+  root.appendChild(codeDiv);
+}
+
+//showCode("", 1);
